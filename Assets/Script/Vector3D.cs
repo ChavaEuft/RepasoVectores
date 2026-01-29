185e4a3d8453a06a36199945fa3a6f6277b95ae6
ChavaEuft <sechat@alumnatflorida.es>
2026-01-29 09:58:32 +0100

    a

---
+++ Assets/Script/Vector3D.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class Vector3D : MonoBehaviour
+{
+    float x;
+    float y;
+    float z;
+
+    public Vector3D()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    public Vector3D(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
